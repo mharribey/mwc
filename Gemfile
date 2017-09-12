5519372ca189fa39ad0dbe41484e0e6173bbd84e
mharribey <mathieuharribey@hotmail.fr>
2017-09-12 15:52:41 +0200

    add wagon middleman

--- Gemfile
+++ Gemfile
@@ -1,6 +1,12 @@
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 4.2'
-gem 'middleman-autoprefixer', '~> 2.7'
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+gem 'middleman', '~> 3.4'
+
+# Deploy to GitHub Pages
+gem 'middleman-deploy', '~> 1.0'
+
+# Assets
+gem 'jquery-middleman'
+gem 'bootstrap-sass', '3.3.6'
+gem 'font-awesome-sass', '~> 4.7.0'
+gem 'middleman-autoprefixer'
--- config.rb
+++ config.rb
@@ -1,46 +1,91 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+###
+# Compass
+###
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
+# Auto-prefixing of CSS code with vendor prefix
+activate :autoprefixer
 
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+###
+# Page options, layouts, aliases and proxies
+###
 
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
 # With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
+###
 # Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
+###
 
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
-#     'Helping'
+#     "Helping"
 #   end
 # end
 
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+set :css_dir, 'stylesheets'
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+  #
+  require_relative "./lib/build_cleaner"
+  activate :build_cleaner
+end
+
+# Deployment
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+
+  # Optional Settings
+  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
